4d696bf9b15dd98af771a74f6fbad48bd3b4f9e7
nicocaputocai <nicolas_caputo@hotmail.com>
2020-09-12 00:35:05 -0300

    terminé el show/editor de productos y cree ruta delete

--- Site/controllers/adminController.js
+++ Site/controllers/adminController.js
@@ -1,9 +1,64 @@
+const fs = require('fs')
+const path = require('path');
+const dbProducts = require('../data/dbProducts')
+
+
 module.exports = {
-    admin:function(req,res){
+    agregar:function(req,res){
        // http://localhost:3000/admin
         res.render('admin', { 
             title: 'Admin | Buenas Tintas' ,
             css: 'admin.css'
         })
+    },
+    show:function(req,res){
+        let idProducto = req.params.id;
+        let flap = req.params.flap;
+        let activeDetail;
+        let activeEdit;
+        let showDetail;
+        let showEdit;
+
+        if(flap=="show"){
+            activeDetail = 'active';
+            showDetail = 'show';
+        }else{
+            activeEdit = 'active';
+            showEdit = 'show';
+        }
+        let resultado = dbProducts.filter(producto =>{
+            return producto.id == idProducto
+        })
+        res.render('vistaProducto',{
+            title: 'Ver / Editar | BT',
+            css: 'tienda.css',
+            total: dbProducts.lenght,
+            producto:resultado[0],
+            activeDetail: activeDetail,
+            activeEdit:activeEdit,
+            showDetail: showDetail,
+            showEdit:showEdit,
+        })
+    },
+    editar: function(req,res){
+        let idProducto = req.params.id;
+        dbProducts.forEach(producto => {
+            if(producto.id == idProducto){
+                producto.id = Number(req.body.id),
+                producto.bodega = req.body.bodega,
+                producto.nombre = req.body.nombre,
+                producto.varietal = req.body.varietal,
+                producto.año = req.body.año,
+                producto.price = Number(req.body.price),
+                producto.discount = Number(req.body.discount),
+                producto.category = req.body.category,
+                producto.description = req.body.description,
+                producto.image = producto.image
+            }
+        })
+        fs.writeFileSync(path.join(__dirname,'../data/productsDataBase.json'),JSON.stringify(dbProducts),'utf-8');
+        res.redirect('/admin/show/'+ idProducto +'/show')
+    },
+    eliminar: function(req,res){
     }
 }
--- Site/routes/admin.js
+++ Site/routes/admin.js
@@ -3,6 +3,12 @@ const adminController = require('../controllers/adminController');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', adminController.admin)
+router.get('/', adminController.agregar)
+/* Ver / Editar */
+router.get('/show/:id/:flap?',adminController.show);
+/* Editar producto */
+router.put('/edit/:id', adminController.editar)
+/* Eliminar producto */
+router.delete('/delete/:id', adminController.eliminar)
 
 module.exports = router;
