2b4acc5b1a8328da38394417faa6c8648f5e30e0
MilagrosGaundri <miligaundri10mail.com>
2020-09-30 18:10:48 -0300

    Formulario de registro,encriptar contraseñas y agregar fotos, funcionando

--- Site/controllers/usersController.js
+++ Site/controllers/usersController.js
@@ -1,4 +1,9 @@
-const adminController = require("./adminController")
+
+const path = require('path');
+const fs = require('fs');
+
+const dbUsers = require(path.join(__dirname,'..','data','dbUsers'))
+const bcrypt = require('bcrypt');
 
 module.exports = {
      // http://localhost:3000/users/
@@ -14,5 +19,26 @@ module.exports = {
             title: 'Registro | Buenas Tintas',
             css: 'users.css'
         })
+    },
+    agregarRegister: (req,res)=>{
+        let idNuevo = 1;
+        dbUsers.forEach(users=>{
+           if(users.id > idNuevo){
+               idNuevo = users.id
+           }
+        })
+        let nuevoUsers={
+            id: idNuevo + 1,
+            categoría:'usuario',
+            nombreCompleto: req.body.nombreCompleto.trim(),
+            correo:req.body.correo.trim(),
+            nacimiento: req.body.nacimiento,
+            image:(req.files[0])?req.files[0].filename:"foto-undefined.png",
+            telefono:Number(req.body.telefono),
+            contraseña:bcrypt.hashSync(req.body.contraseña,10),
+        }
+        dbUsers.push(nuevoUsers);
+        fs.writeFileSync(path.join(__dirname,"..","data","usersDataBase.json"),JSON.stringify(dbUsers),'utf-8')
+        res.redirect('/users')
     }
 }
--- Site/routes/users.js
+++ Site/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController')
+const upImagesUsers = require('../middlewares/upImagesUsers')
 
 /* GET users listing. */
 router.get('/', usersController.login)
 router.get('/register', usersController.register)
+router.post('/register',upImagesUsers.any(), usersController.agregarRegister)
 
 module.exports = router;
