3070636c1d3f102a7bcbaeefe67609de0213bebd
nicocaputocai <nicolas_caputo@hotmail.com>
2020-10-04 18:44:28 -0300

    Al carrito solo se entra logueado

--- Site/middlewares/loginPassMW.js
+++
@@ -1,9 +0,0 @@
-function adminMW(req,res,next){
-    if (req,session != 'undefined'){
-        next()
-    }else{
-        res.redirect('/')
-    }
-}
-
-module.exports = loginPassMW
--- Site/routes/tienda.js
+++ Site/routes/tienda.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const tiendaController = require('../controllers/tiendaController')
-
+const logMiddleware = require('../middlewares/logMiddleware')
 
 /* GET home page. */
 router.get('/', tiendaController.tienda);
-router.get('/carrito',  tiendaController.carrito);
+router.get('/carrito', logMiddleware, tiendaController.carrito);
 router.get('/search',tiendaController.search)
 router.get('/:id', tiendaController.detalle);
 
