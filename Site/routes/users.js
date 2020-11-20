ae65679f986ae6324d016e14d02bff0ed63d831f
MilagrosGaundri <miligaundri10@gmail.com>
2020-11-20 18:58:11 -0300

    validacion de registro back-end y imagenes de tienda niveladas

--- Site/routes/users.js
+++ Site/routes/users.js
@@ -3,12 +3,14 @@ var router = express.Router();
 const usersController = require('../controllers/usersController')
 const upImagesUsers = require('../middlewares/upImagesUsers')
 const loginValidator = require('../validators/loginValidator');
+const registerValidator = require('../validators/registerValidator')
 const logMiddleware = require('../middlewares/logMiddleware')
+// const dataMV = require('../middlewares/dateMW')
 
 /* GET users listing. */
 
 router.get('/register', usersController.register)
-router.post('/register',upImagesUsers.any(), usersController.agregarRegister)
+router.post('/register',upImagesUsers.any(), registerValidator ,usersController.agregarRegister)
 
 router.get('/login', usersController.login) //ejecuta el metodo para cargar la vista del formulario....
 router.post('/login',loginValidator,usersController.processLogin) //ejecuta el metodo que procesa lo que se envía del formulario
---
+++ Site/validators/registerValidator.js
@@ -0,0 +1,60 @@
+const {check,validationResult,body} = require('express-validator');
+
+let db = require('../database/models');
+
+module.exports= [
+    check('name')
+    .isLength({
+        min:1
+    })
+    .withMessage("Debes ingresar tu nombre"),
+
+    check('last_name')
+    .isLength({
+        min:1
+    })
+    .withMessage("Debes ingresar tu apellido"),
+
+    check('email')
+    .isEmail()
+    .withMessage("Debes ingresar un email válido"),
+
+    body('email')
+    .custom(function(value){
+        return db.Users.findOne({
+            where:{
+                email:value
+            }
+        })
+        .then(user=>{
+            if(user){
+                return Promise.reject('El email ingresado ya esta existente')
+            }
+        })
+    }),
+    
+    check('date')
+    .isLength({
+        min:1
+    })
+    .withMessage('Debes ser mayor de 18 años'),
+
+    
+    check('phone')
+    .isLength({
+        min:8
+    })
+    .withMessage('Debes ingresar un numero de teléfono'),
+
+
+    check('password')
+    .isLength({
+        min:6,
+        max:12
+    })
+    .withMessage("Debes ingresar una contraseña entre 6 y 12 caracteres"),
+
+    check('bases')
+    .isString("on")
+    .withMessage("Debes aceptar las bases y condiciones")
+]
