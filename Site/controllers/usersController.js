d806fc7439566bf305f48c5aebd2d93aa71646f0
nicocaputocai <nicolas_caputo@hotmail.com>
2020-12-03 16:13:10 -0300

    login corregido

--- Site/controllers/usersController.js
+++ Site/controllers/usersController.js
@@ -12,7 +12,7 @@ module.exports = {
         res.render('users', { 
             title: 'Login | Buenas Tintas',
             css: 'users.css',
-            scripts :'userLogin.js'
+            script :'userLogin.js'
         })
     },
     // http://localhost:3000/users/register
@@ -20,7 +20,7 @@ module.exports = {
         res.render('register', { 
             title: 'Registro | Buenas Tintas',
             css: 'users.css',
-            script : 'registerUser.js'
+            script :'registerUser.js'
         })
     },
     processLogin:function(req,res){
